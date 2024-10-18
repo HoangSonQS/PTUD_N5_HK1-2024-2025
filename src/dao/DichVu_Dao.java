64294baba727a22e8d33fd943c5932672b80498c
tiennguyen2406 <142229123+tiennguyen2406@users.noreply.github.com>
2024-10-18 17:38:59 +0700

    Add files via upload

--- src/dao/DichVu_Dao.java
+++ src/dao/DichVu_Dao.java
@@ -10,7 +10,7 @@ import connectDB.ConnectDB;
 import entity.DichVu;
 
 public class DichVu_Dao {
-	public static ArrayList<DichVu> getAllDichVu(){
+	public ArrayList<DichVu> getAllDichVu(){
 		ArrayList<DichVu>dsDV = new ArrayList<DichVu>();
 		Connection conN = ConnectDB.getInstance().getConnection();
 		Statement stm = null;
@@ -99,7 +99,7 @@ public class DichVu_Dao {
 			return false;
 		}
 	}
-	public static DichVu layDichVuTheoMa(String idDichVu) {
+	public DichVu layDichVuTheoMa(String idDichVu) {
 	    DichVu dv = null;
 	    Connection con = ConnectDB.getInstance().getConnection();
 	    PreparedStatement stmt = null;
