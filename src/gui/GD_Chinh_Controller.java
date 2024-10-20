346921d32ef998bb4d098df202e1d6c8625ae5b4
unknown <sonjiny2004@gmail.com>
2024-10-20 22:58:00 +0700

    Nhieu Loi Vai Cut

--- src/gui/GD_Chinh_Controller.java
+++ src/gui/GD_Chinh_Controller.java
@@ -2,19 +2,31 @@ package gui;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.Objects;
 import java.util.ResourceBundle;
+
+import javax.swing.plaf.RootPaneUI;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.TreeView;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
 import main.App;
 
 
 public class GD_Chinh_Controller implements Initializable{
+
+    @FXML
+    private ImageView avt;
 
     @FXML
     private ImageView icon_QuanLy;
@@ -39,10 +51,19 @@ public class GD_Chinh_Controller implements Initializable{
 
     @FXML
     private Label lb_TimKiem;
+
+    @FXML
+    private Pane pn_NoiDung;
+
+	private Stage primaryScene;
+
+	private Stage primaryStage;
+
+	private Node pnNoiDung;
     
     @FXML
     void moGiaoDienQuanLy(MouseEvent event) throws IOException {
-    	App.setRoot("GD_QLPhong");
+    	App.setRoot("GD_QLNhanVien");
     }
 
     @FXML
@@ -56,13 +77,11 @@ public class GD_Chinh_Controller implements Initializable{
     }
 
     @FXML
-    void moGiaoDienTimKiem(MouseEvent event) {
-
+    void moGiaoDienTimKiem(MouseEvent event) throws IOException {
+    	App.setRoot("GD_ThongKeDoanhThu");
     }
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
-		// TODO Auto-generated method stub
-		
 	}
 
 }
--- src/gui/TestJavaFX.java
+++ src/gui/TestJavaFX.java
@@ -11,7 +11,7 @@ public class TestJavaFX extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			Parent root = FXMLLoader.load(getClass().getResource("GD_DangNhap.fxml"));
+			Parent root = FXMLLoader.load(getClass().getResource("GD_QLNhanVien.fxml"));
 			Scene scene = new Scene(root);
 
 			primaryStage.setScene(scene);
