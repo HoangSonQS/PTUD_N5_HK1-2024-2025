339b8ecb28e9e7030ba3b88155d3e7fe315bd4d9
unknown <sonjiny2004@gmail.com>
2024-10-22 17:08:40 +0700

    son

--- src/gui/GD_QLPhong_Controller.java
+++ src/gui/GD_QLPhong_Controller.java
@@ -1,93 +1,57 @@
 package gui;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableView;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 
-public class GD_QLPhong_Controller {
+public class GD_QLPhong_Controller implements Initializable{
 
-    @FXML
-    private Button btnSua1;
+	 	@FXML
+	    private ImageView avt;
 
-    @FXML
-    private Button btnSua2;
+	    @FXML
+	    private Button btnSua1;
 
-    @FXML
-    private Button btnThem;
+	    @FXML
+	    private Button btnSua2;
 
-    @FXML
-    private Button btnXem;
+	    @FXML
+	    private Button btnThem;
 
-    @FXML
-    private Button btnXoa1;
+	    @FXML
+	    private Button btnXoa1;
 
-    @FXML
-    private Button btnXoa2;
+	    @FXML
+	    private Button btnXoa2;
 
-    @FXML
-    private Button btn_TimPhong;
+	    @FXML
+	    private Button btn_TimPhong;
 
-    @FXML
-    private ImageView img_KieuPhong;
+	    @FXML
+	    private ComboBox cbb;
 
-    @FXML
-    private Label lb_GiaPhong;
+	    @FXML
+	    private TableView<?> tablePhong;
 
-    @FXML
-    private Label lb_LoaiPhong;
+	    @FXML
+	    void nhanNutSua(ActionEvent event) {
 
-    @FXML
-    private Label lb_MaPhong;
-
-    @FXML
-    private TableView<?> tablePhong;
-
-    @FXML
-    void nhanNutSua(ActionEvent event) {
-
-    }
-
-    @FXML
-    void nhanNutThem(ActionEvent event) {
-
-    }
-
-    @FXML
-    void nhanNutXem(ActionEvent event) {
-
-    }
-
-    @FXML
-    void nhanNutXoa(ActionEvent event) {
-
-    }
-
-    @FXML
-    void setGiaPhong(MouseEvent event) {
-
-    }
-
-    @FXML
-    void setKieuPhong(MouseEvent event) {
-
-    }
-
-    @FXML
-    void setLoaiPhong(MouseEvent event) {
-
-    }
-
-    @FXML
-    void setMaPhong(MouseEvent event) {
-
-    }
-
-    @FXML
-    void timPhong(ActionEvent event) {
-
-    }
-
+	    }
+	    
+	    @Override
+		public void initialize(URL arg0, ResourceBundle arg1) {
+			ObservableList<String> list = FXCollections.observableArrayList("Tất cả", "Phòng đơn", "Phòng đôi", "Phòng gia đình");
+			cbb.setItems(list);
+			cbb.setValue("Tất cả");
+	    }
 }
