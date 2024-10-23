a54fda0fd14eaf982a2b2a9ffd7d23c450cfef30
tiennguyen2406 <khaitien600@gmail.com>
2024-10-23 10:11:40 +0700

    t

--- src/gui/GD_DatPhongChoController.java
+++ src/gui/GD_DatPhongChoController.java
@@ -1,39 +1,29 @@
 package gui;
 
+
+
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.DatePicker;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.AnchorPane;
+import javafx.scene.control.*;
 
-public class GD_DatPhong_Controller {
-	 	@FXML
-	    private AnchorPane GD_DatPhongCho;
+public class GD_DatPhongChoController {
+    @FXML
+    private TextField txtMaPhong;
+    @FXML
+    private TextField txtSDT;
+    @FXML
+    private TextField txtKH;
+    @FXML
+    private TextField txtCCCD;
+    @FXML
+    private DatePicker dpNgayNhan;
+    @FXML
+    private DatePicker dpNgaySinh;
+    @FXML
+    private Button btnClose;
+    @FXML
+    private Button btnRefresh;
+    @FXML
+    private Button btnBookWaitingRoom;
 
-	    @FXML
-	    private Button btnBookWaitingRoom;
 
-	    @FXML
-	    private Button btnClose;
-
-	    @FXML
-	    private Button btnRefresh;
-
-	    @FXML
-	    private DatePicker dpNgayNhan;
-
-	    @FXML
-	    private DatePicker dpNgaySinh;
-
-	    @FXML
-	    private TextField txtCCCD;
-
-	    @FXML
-	    private TextField txtKH;
-
-	    @FXML
-	    private TextField txtMaPhong;
-
-	    @FXML
-	    private TextField txtSDT;
 }
